1e04504437da556ea37192f2452ceb1b314a1d52
姚智豪 <yaozhihao@yaozhihaodeAir.lan>
2018-06-14 12:32:23 +0800

    第一次上传

---
+++ Blindata/General/Categories/UIViewController+Base.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Base.h
+//  Blindata
+//
+//  Created by YZH on 2018/6/12.
+//  Copyright © 2018年 YZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SLiderMenuViewController.h"
+@interface UIViewController (Base)
+
+@property (nonatomic,readonly) SLiderMenuViewController *slideMenuController;
+
+@end
